3c2cc8510855bf0a8e840ac05171cfcc1edaef32
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-08 13:31:59 -0600

    fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Notiflix from 'notiflix';
+// import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
@@ -10,27 +10,27 @@ fetch('https://pixabay.com/api/?key=41150713-bfede5582ab5c0a4976e7b783')
   .then(response => response.json())
   .then(console.log());
 
-axios
-  .get('URL')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+// axios
+//   .get('https://pixabay.com/api/?key=41150713-bfede5582ab5c0a4976e7b783')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // always executed
+//   });
 
 //   skroll
 
-const { height: cardHeight } = document
-  .querySelector('.gallery')
-  .firstElementChild.getBoundingClientRect();
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: 'smooth',
-});
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
