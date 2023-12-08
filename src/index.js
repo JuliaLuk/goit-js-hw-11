62700e8d45b961f601d1ff62acd0fc524d796676
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-08 12:00:56 -0600

    added axios

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-const axios = require('axios/dist/node/axios.cjs');
-// import Notiflix from 'notiflix';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = 41150713 - bfede5582ab5c0a4976e7b783;
+
+axios
+  .get('url')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+//   skroll
+
+const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
