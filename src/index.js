3435102fe59e3dabbc569bded6b39cdf5e592dfa
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-08 13:06:20 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 
 const URL = 'https://pixabay.com/api/';
-const API_KEY = 41150713 - bfede5582ab5c0a4976e7b783;
+const API_KEY = '41150713 - bfede5582ab5c0a4976e7b783';
+
+fetch('https://pixabay.com/api/?key=41150713-bfede5582ab5c0a4976e7b783')
+  .then(response => response.json())
+  .then(console.log());
 
 axios
-  .get('url')
+  .get('URL')
   .then(function (response) {
     // handle success
     console.log(response);
